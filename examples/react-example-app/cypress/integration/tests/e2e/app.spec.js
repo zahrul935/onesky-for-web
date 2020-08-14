888fd39328d624380a0521356819ab7610936aad
Neha Pandita <nehapandita@outlook.com>
2020-08-14 14:01:59 +0530

    feat: cypress end-2-end tests 1. Added Cypress package to packages.json 2. Added test for language selector

---
+++ examples/react-example-app/cypress/integration/tests/e2e/app.spec.js
@@ -0,0 +1,15 @@
+describe('the home page', () => {
+    it('successfully loads', () => {
+        cy.visit('onesky.clickvalley.in')
+    })
+    it('select language', () => {
+        cy.get('body > language-selector > select').select('简体中文 (中国)')
+        cy.contains('简单句子')
+        cy.contains('Message Not in Component Example: react-intl-universal 能够使消息不在 React.Component 中国际化')
+    })
+    it('select different lenguage', () => {
+        cy.get('body > language-selector > select').select('English (US)')
+        cy.contains('Simple Sentence')
+        cy.contains('This is <script>alert("ReactIntlUniversal prevents from xss attack")</script>')
+    })
+})
