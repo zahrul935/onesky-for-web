da83466ba7ca2421f0ee7b79f9df82b2ad8d65f1
zahrul935 <62903622+zahrul935@users.noreply.github.com>
2020-09-19 04:38:44 +0700

    Update language-selector.spec.js

--- examples/react-example-app/cypress/integration/tests/e2e/language-selector.spec.js
+++ examples/react-example-app/cypress/integration/tests/e2e/language-selector.spec.js
@@ -3,12 +3,12 @@ describe('the home page', () => {
         cy.visit('onesky.clickvalley.in')
     })
     it('select language', () => {
-        cy.get('body > language-selector > select').select('简体中文 (中国)')
+        cy.get('body > language-selector > select').select(indonesia )')
         cy.contains('简单句子')
-        cy.contains('Message Not in Component Example: react-intl-universal 能够使消息不在 React.Component 中国际化')
+        cy.contains('Message Not in Component Example: react-intl-universal indonesia  React.Component indonesia )
     })
     it('select different language', () => {
-        cy.get('body > language-selector > select').select('English (US)')
+        cy.get('body > language-selector > select').select('indonesia  (indonesia )')
         cy.contains('Simple Sentence')
         cy.contains('This is <script>alert("ReactIntlUniversal prevents from xss attack")</script>')
     })
